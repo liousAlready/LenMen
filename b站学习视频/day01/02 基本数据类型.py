e6874278ebba3b438b65ca7cf1935873814c0089
limusen <16@qq.com>
2022-02-23 20:25:04 +0800

    add

---
+++ b站学习视频/day01/02 基本数据类型.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/2/23 19:43
+# @Author : Limusen
+# @File : 02 基本数据类型
+
+
+# 1.数据类型
+# 1.1 整形int
+# 作用：记录年龄、个数等等
+# 定义：
+age = 19
+# print(type(age))
+
+# 1.2浮点型float
+# 作用：记录薪资、身高、体重
+# 定义
+salary = 3.3
+height = 1.87
+weight = 80.3
+# print(type(height))
+
+# 数字类型的其他使用
+level = 1
+level = level + 1
+# print(level)
+
+print(10 + 11.2)  # int 与float之间是可以相加的
+
+# 2.字符串类型 str
+# 作用: 记录描述性质的状态,名字/一段话
+# 定义: 在引号内 ('',"",''' ''',""" """,) 包含的一串字符串
+info = '''
+爱酸酸的啊少的可怜就阿萨德
+'''
+# print(type(info))
+# name ="egon"
+# print(egon)
+
+
