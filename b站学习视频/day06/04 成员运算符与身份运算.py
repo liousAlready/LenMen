eed4024bece887fb5fc8f724c07c474bfa9da2ec
limusen <m15574933885@163.com>
2022-03-06 10:09:19 +0800

    add

---
+++ b站学习视频/day06/04 成员运算符与身份运算.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/6 10:00
+# @Author  : limusem
+# @File    : 04 成员运算符与身份运算.py
+# @Software: PyCharm
+# @Description:
+
+# 1.成员运算符
+
+# print("egon" in "hello egon") # 判断一个字符串是否存在一个大字符串当中
+
+# print(111 in [111,222,333]) # 判断元素是否存在于列表
+
+# 判断key 是否存在于字典
+# print(111 in {"k1":111,"k2":222})
+
+
+# not in
+print("egon " not in "hello egon")
+print(not "egon " in "hello egon")
+
+# 2.身份运算符
+
+# is 判断id是否相等
---
+++ b站学习视频/day06/05 流程控制if判断.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/6 10:05
+# @Author  : limusem
+# @File    : 05 流程控制if判断.py
+# @Software: PyCharm
+# @Description:
+
+
+"""
+
+语法1：
+
+if 条件：
+    代码1
+    代码2
+    代码3
+else：
+    代码
+
+
+"""
